b9b2d8ba11ddf1d3832a2fbce3b87de60b22fc10
hurrisonferd <150211906+hurrisonferd@users.noreply.github.com>
2026-09-17 20:07:25 -0400

    build(android): bump RavenOS Home to 0.2.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.ravenos.home"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.1.1"
+        versionCode = 3
+        versionName = "0.2.0"
     }
 
     sourceSets["main"].assets.srcDir("../../site")
